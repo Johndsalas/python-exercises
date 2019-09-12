612a3f8d6d789478172f3c2eaa422f39fc39b622
John Salas <johndsalas@gmail.com>
2019-09-11 19:26:10 -0500

    new push

--- 4.3_control_structures_exercises.py
+++ 4.3_control_structures_exercises.py
@@ -168,3 +168,259 @@ while x <= 9:
 
 #break and continue
 
+"""break and continue
+
+Prompt the user for an odd number between 1 and 50. 
+Use a loop and a break statement to continue prompting the user if they enter invalid input. 
+(Hint: use the isdigit method on strings to determine this). 
+Use a loop and the continue statement to output all the odd numbers between 1 and 50, except for the number the user entered.
+"""
+
+
+gate_keeper = True
+
+while gate_keeper == True:
+    
+    print("Odd number, please?")
+    number = input()
+    
+    if number.isdigit() == True:
+        number = int(number)
+        
+        if number % 2 != 0:
+            break
+            
+i = 1
+
+print(f"The number being skipped is: {number}")
+print("")
+
+while i <= 50:
+    
+    if i == 1 and number == 1:
+        print(f"Yikes! Skipping number: {i}")
+        i += 1
+        continue
+    
+    if i % 2 == 0: 
+        i += 1
+        continue
+        
+    if i == number:
+        print(f"Yikes! Skipping number: {i}")
+        i += 1
+        continue
+    
+    print(f"Here is an odd number: {i} ")
+    i += 1
+
+"""
+The input function can be used to prompt for input and use that input in your python code. 
+Prompt the user to enter a positive number and write a loop that counts from 0 to that number. 
+(Hints: first make sure that the value the user entered is a valid number, also note that the input function 
+returns a string, so you'll need to convert this to a numeric type.)
+
+"""
+
+gate_keeper = True
+count = 0 
+
+while gate_keeper == True:
+
+    print("Positive number, please?")
+    number = input()
+    print("")
+    
+    if number.isdigit() == True:
+        number = int(number)
+        
+        if number > 0:
+            break
+            
+while count <= number:
+    
+    print(count)
+    count += 1
+
+
+
+#Write a program that prompts the user for a positive integer. Next write a loop that prints out the numbers from the number the user entered down to 1.
+
+
+gate_keeper = True
+
+while gate_keeper == True:
+    
+    print("Positive number, please?")
+    number = input()
+    print("")
+    
+    if number.isdigit() == True:
+        number = int(number)
+        
+        if number > 0:
+            break
+            
+while number > 0:
+    
+    print(number)
+    number -= 1
+
+Fizzbuzz
+
+'''
+One of the most common interview questions for entry-level programmers is the FizzBuzz test. 
+Developed by Imran Ghory, the test is designed to test basic looping and conditional logic skills.
+
+Write a program that prints the numbers from 1 to 100.
+For multiples of three print "Fizz" instead of the number
+For the multiples of five print "Buzz".
+For numbers which are multiples of both three and five print "FizzBuzz".
+
+'''
+count = 1
+
+while count <= 100:
+
+    if count % 3 == 0 and count % 5 == 0:
+        print("FizzBuzz")
+    
+    elif count % 3 == 0:
+        print("Fizz")
+
+    elif count % 5 == 0:
+        print("Buzz")
+
+    else: print(count)
+
+    count += 1
+
+'''
+Display a table of powers.
+Prompt the user to enter an integer.
+Display a table of squares and cubes from 1 to the value entered.
+Ask if the user wants to continue.
+Assume that the user will enter valid data.
+Only continue if the user agrees to.
+Example Output
+
+What number would you like to go up to? 5
+
+Here is your table!
+
+number | squared | cubed
+------ | ------- | -----
+1      | 1       | 1
+2      | 4       | 8
+3      | 9       | 27
+4      | 16      | 64
+5      | 25      | 125
+
+Bonus: Research python's format string specifiers to align the table
+'''
+
+
+gate_keeper = True
+
+while gate_keeper == True:
+    
+    print("What number do you want to go to?")
+    number = input()
+
+    count = 1
+    number = int(number)
+
+    print("")
+    print("number | squared | cubed")
+    print("------ | ------- | -----")
+
+    while count <= number:
+    
+        print("{:7}|{:9}|{:6}".format(count, count ** 2, count ** 3))
+    
+        count += 1
+    
+    print("")
+    print("Do you wish to continue?")
+    wish = input()
+    
+    if wish.lower() != "yes":
+        gate_keeper = False
+        
+ '''
+Convert given number grades into letter grades.
+
+Prompt the user for a numerical grade from 0 to 100.
+Display the corresponding letter grade.
+Prompt the user to continue.
+Assume that the user will enter valid integers for the grades.
+The application should only continue if the user agrees to.
+Grade Ranges:
+
+A : 100 - 88
+B : 87 - 80
+C : 79 - 67
+D : 66 - 60
+F : 59 - 0
+Bonus
+
+Edit your grade ranges to include pluses and minuses (ex: 99-100 = A+).
+'''
+
+gate_keeper = True
+
+while gate_keeper == True:
+    
+    print("Enter numerical grade.")
+    grade = input()
+    grade = int(grade)
+
+    if grade in range(99,101):
+        print("A+")
+    
+    if grade in range(92,99):
+        print("A")
+        
+    if grade in range(90,92):
+        print("A-")
+        
+    if grade in range(88,90):
+        print("B+")
+        
+    if grade in range(82,89):
+        print("B")
+        
+    if grade in range(80,82):
+        print("B-")
+        
+    if grade in range(78,80):
+        print("C+")
+    
+    if grade in range(72,78):
+        print("C")
+        
+    if grade in range(70,72):
+        print("C-")
+        
+    if grade in range(68,70):
+        print("D+")
+        
+    if grade in range(62,68):
+        print("D")
+    
+    if grade in range(60,62):
+        print("D-")
+        
+    if grade in range(1,60):
+        print("F")
+    
+    print("Would you like to input another grade? yes or no")
+    again = input()
+
+    if again.lower() != "yes":
+        gate_keeper = False
+    
+
+Create a list of dictionaries where each dictionary represents a book that you have read. Each dictionary in the list should have the keys title, author, and genre. Loop through the list and print out information about each book.
+
+Prompt the user to enter a genre, then loop through your books list and print out the titles of all the books in that genre.
