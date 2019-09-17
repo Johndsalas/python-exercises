c6911ed59e033717934f3cb48908aaaeaf5015e2
John Salas <johndsalas@gmail.com>
2019-09-16 21:09:29 -0500

    WIP

--- checkbook.py
+++ checkbook.py
@@ -1,3 +1,5 @@
+application = "on"
+
 print("~~~ Welcome to your terminal checkbook! ~~~")
 
 
@@ -5,6 +7,10 @@ while application == "on":
 
     print("")
     print("")
+
+
+
+
     print("What would you like to do?")
     print("")
     print("1: View Balance")
@@ -12,21 +18,26 @@ while application == "on":
     print("3: Record a Credit (Deposit)")
     print("4: Quit Application")
     print("")
+    choice = input()
 
     if choice == "1" or "2" or "3" or "4":
         choice = int(input())
 
     if choice == "1":
-        balance == "on"
+        print("1")
         
     elif choice == "2":
-        debit == "on"
+        print("2")
     
     elif choice == "3":
-        credit == "on"
+        print("3")
     
     elif choice == "4":
-        
+        print(4)
+        print("END DEMO")
+        break
+
+ '''       
         print("")
         print("")
         print("Thanks, have a great day!")
@@ -60,32 +71,59 @@ while application == "on":
             print("")
             print(f"Your current balance is ${balance}.")
             print("")
-            print("How much are you withdrawing?")
+            print("Amount to debit? (withdraw)")
             
             withdraw = input()
             withdraw = int(withdraw)
             
             #subtract withdraw from balance
             
-            print(f"You are withdrawing ${withdraw}. Your current balance is now {balance}.)
+            print(f"You are withdrawing ${withdraw}. Your current balance is now ${balance}.)
             print("")
             print("")
-            print("Would you like another tranaction? y/n")      
+            print("Would you like another tranaction? y/n")    
+
+            debit_choice = input()
+            
+            if debit_choice == n:
+                choice = "4"
+                
+            if debit_choice == y:
+                choice = "null"
+                
+            else:
+                
+                print("Invalid entry. Plese type 'y' or 'n'")
             
         while choice == "3":
                 
             print("")
             print(f"Your current balance is ${balance}.")
             print("")
-            print("How much are you withdrawing?")
+            print("Amount to credit?")
             
-            withdraw = input()
-            withdraw = int(withdraw)
+            credit= input()
+            credit= int(credit)
             
-            #subtract withdraw from balance
+            #add to balance
             
-            print(f"You are withdrawing ${withdraw}. Your current balance is now {balance}.)
+            print(f"You are crediting ${credit}. Your current balance is now {balance}.)
             print("")
             print("")
             print("Would you like another tranaction? y/n")      
                 
+            credit_choice = input()
+            
+            if credit_choice == n:
+                choice = "4"
+                
+            if debit_choice == y:
+                choice = "null"
+                
+            else:
+                
+                print("Invalid entry. Plese type 'y' or 'n'")
+
+        if choice == 4:
+
+            print("Thank You, Have a nice day")
