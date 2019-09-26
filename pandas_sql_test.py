235f99d3d50fb773360a87f5b62069c5f1a5e9a4
John Salas <johndsalas@gmail.com>
2019-09-26 16:56:42 -0500

    commit

--- pandas_sql_test.py
+++ pandas_sql_test.py
@@ -3,3 +3,21 @@ from env import host, user, password
 url = f'mysql+pymysql://{user}:{password}@{host}/employees'
 df = pd.read_sql('SELECT * FROM employees LIMIT 5 OFFSET 50', url)
 print(df)
+
+
+import pandas as pd
+from env import host, user, password
+
+database_name = "sakila"
+
+url = f'mysql+pymysql://{user}:{password}@{host}/{database_name}'
+
+query = """ SELECT * FROM actor WHERE first_name = 'penelope' """
+
+df = pd.read_sql(query, url)
+
+print(df)
+
+
+groupby != dataframe unless .agg('measurement')
+
